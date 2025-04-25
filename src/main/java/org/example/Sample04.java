b3b4b827f2c13d0710abb4129e8502ba15f76400
Tomari_Home <daytomari@gmail.com>
2025-04-26 00:21:30 +0900

    サンプル4修正

--- src/main/java/org/example/Sample04.java
+++ src/main/java/org/example/Sample04.java
@@ -7,13 +7,13 @@ import java.nio.file.StandardOpenOption;
 
 public class Sample04 {
     public static void main(String[] args) throws IOException {
-        try {
+//        try {
 //            Path path = Path.of(first:"JavaCourse.txt");
 //            Files.writeString(path, "Javaコース楽しい！", StandardOpenOption.APPEND);
-        } catch (IOException e) {
-            System.out.println("例外が発生しました。");
-        } finally {
-            System.out.println("無事に処理が終了しました。");
-        }
+//        } catch (IOException e) {
+//            System.out.println("例外が発生しました。");
+//        } finally {
+//            System.out.println("無事に処理が終了しました。");
+//        }
     }
 }
