26e8bc972b952130364c035b8230ed8341ac9472
daytomari <tomari.daigorou@link-academy.co.jp>
2025-04-25 21:46:06 +0900

    Initial commit

---
+++ src/main/java/org/example/Sample01.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Sample01 {
+    public static void main(String[] args) {
+        String firstName="太郎";
+        String lastName="凛空";
+
+        if (firstName.length() >= 2 && lastName.length() >= 2) {
+            System.out.println(lastName + "　" + firstName);
+        } else {
+            System.out.println("条件に一致しませんでした。");
+        }
+    }
+}
